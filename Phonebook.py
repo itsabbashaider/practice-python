a58e3b7b822c2621b4675d3b9e64b9cee1e2b19d
Abbas-Haider-5 <its.abbas.hyder>
2025-08-10 10:49:19 +0500

    Corrected the issues

--- Phonebook.py
+++ Phonebook.py
@@ -1,60 +1,61 @@
-Phonebook = {"Abbas"    : 317,
-             "Haider"   : 318,
-             "Ali"      : 319,
-             "Muhammad" : 320}
+Phone_book = {}
 
 
 def read_contact():
     name = input("Enter name: ")
-    if name in Phonebook:
-        print(f"{name}: {Phonebook[name]}")
+    if name in Phone_book:
+        print(f"{name}: {Phone_book[name]}")
     else:
         print("Contact does not exist")
 
 def edit_contact():
     name = input("Enter the name for edit: ")
-    if name in Phonebook:
+    if name in Phone_book:
         number = input("Enter the number for edit: ")
-        Phonebook[name] = number
-        print("Edited the Phonebook")
+        Phone_book[name] = number
+        print("Edited the Phone_book")
     else:
         print("Contact does not exist")
+
 def delete_contact():
     name = input("Enter the name you want to delete: ")
-    if name in Phonebook:
-        del Phonebook[name]
+    if name in Phone_book:
+        del Phone_book[name]
         print("The contact is deleted")
     else:
         print("Contact does not exist")
+
 def create_contact():
     name = input("Enter the name to create contact: ")
-    if name in Phonebook:
+    if name in Phone_book:
         print("Contact already exist with this name")
     else:
         number = input("Enter the number for contact: ")
-        Phonebook[name] = number
+        Phone_book[name] = number
         print("Contact has been created")
+
 def list_contacts():
-    if not Phonebook:
+    if not Phone_book:
         print("No contsct found")
     else:
         print("\nAll contacts")
-        for name,number in Phonebook.items():
+        for name,number in Phone_book.items():
             print(f"{name}: {number}")
     
-exit_key = "Q"
 
 while True:
+    print("0.Exit")
     print("1.Read")
     print("2.Edit")
     print("3.Delete")
     print("4.Create")
     print("5.List All")
+   
 
+    Action = input("Select your Action (0/1/2/3/4/5): ")
 
-    Action = input("Select your Action (1/2/3/4/5): ")
-
-
+    if Action == '0':
+        break
     if Action == '1':
         read_contact()
     elif Action == '2':
@@ -67,6 +68,3 @@ while True:
         list_contacts()
     else:
         print("Invalid input")
-    exit_key = (input(f"If you want to exit  ({exit_key}): "))
-    if "Q" in exit_key :
-         break
