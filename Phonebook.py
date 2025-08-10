6469fa93005c9fe4167686edc838adc8afb79e8c
Abbas-Haider-5 <its.abbas.hyder>
2025-08-10 12:22:07 +0500

    Created a txt file to store Phonebook changes

--- Phonebook.py
+++ Phonebook.py
@@ -1,4 +1,19 @@
 Phone_book = {}
+PHONE_BOOK_FILE = "Phonebook.txt" #Veriable for txt file .
+
+def load_contacts():
+    try:#try used for opening and reading the txt file.
+        with open(PHONE_BOOK_FILE, "r") as file: #We call 'r' so we can read contacts from the txt file.
+            for line in file: #Loop on every line of the txt file.
+                name, number = line.strip().split(":")# Here we can remove all white spaces from L/R and separate them with colon (:).
+                Phone_book[name] = number
+    except FileNotFoundError: #If it gives FileNotFoundError problem the except method will run our code without breaking the code.
+        pass #Used pass for indentation requirment.
+
+def save_contacts():
+    with open(PHONE_BOOK_FILE, "w") as file:#We call 'w' so we will be able to write anything in txt file.
+        for name, number in Phone_book.items():#It loop though Phone_book and return name and number pairs.
+            file.write(f"{name},{number}\n") #It write name and number pairs in new lines.
 
 
 def read_contact():
@@ -13,6 +28,7 @@ def edit_contact():
     if name in Phone_book:
         number = input("Enter the number for edit: ")
         Phone_book[name] = number
+        save_contacts()#Call save function to save changes here.
         print("Edited the Phone_book")
     else:
         print("Contact does not exist")
@@ -21,6 +37,7 @@ def delete_contact():
     name = input("Enter the name you want to delete: ")
     if name in Phone_book:
         del Phone_book[name]
+        save_contacts()#Call save function to save changes here.
         print("The contact is deleted")
     else:
         print("Contact does not exist")
@@ -32,6 +49,7 @@ def create_contact():
     else:
         number = input("Enter the number for contact: ")
         Phone_book[name] = number
+        save_contacts()#Call save function to save changes here.
         print("Contact has been created")
 
 def list_contacts():
@@ -41,7 +59,8 @@ def list_contacts():
         print("\nAll contacts")
         for name,number in Phone_book.items():
             print(f"{name}: {number}")
-    
+            
+load_contacts()#Calling this function so while loop loads all the contacts from Phonebook.txt into the Phone_book dictionary.
 
 while True:
     print("0.Exit")
@@ -68,3 +87,5 @@ while True:
         list_contacts()
     else:
         print("Invalid input")
+#I made the changes syntax and the comments to understand what changes i made in the code.
+#And used AI for some errors explaination.
